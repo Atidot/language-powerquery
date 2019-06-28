4e9820272392a084e134030e720e07f987ad25b3
Barak Bercovitz <barakber@gmail.com>
2019-06-28 08:34:31 +0300

    update build

--- build/default.nix
+++ build/default.nix
@@ -1,31 +1,38 @@
-{ nixpkgs  ? import <nixpkgs> { config.allowBroken = true; # for "wasm"
-                              }
+{ nixpkgs  ? import <nixpkgs> { config.allowBroken = true; }
 , compiler ? "ghc864"
 }:
 with nixpkgs;
 let
+  languagePowerQueryASTSrc = ../language-powerquery-ast;
+  languagePowerQuerySrc    = ../language-powerquery;
+  pbixSrc                  = ../pbix;
+  m2wasmSrc                = ../m2wasm;
+
   easeIfGHCJS = package:
     if lib.hasPrefix "ghcjs" compiler
     then haskell.lib.dontHaddock (haskell.lib.dontCheck package)
     else package;
 
-  haskellPackages_ = pkgs.haskell.packages.${compiler}.extend (self: super: rec {
-    lens             = easeIfGHCJS super.lens;
-    comonad          = easeIfGHCJS super.comonad;
-    semigroupoids    = easeIfGHCJS super.semigroupoids;
-    QuickCheck       = easeIfGHCJS super.QuickCheck;
-    tasty-quickcheck = easeIfGHCJS super.tasty-quickcheck;
-    scientific       = easeIfGHCJS super.scientific;
-    temporary        = easeIfGHCJS super.temporary;
-    #--
-    wasm                    = self.callPackage ./wasm.nix {};
-    language-powerquery-ast = self.callPackage ./language-powerquery-ast.nix {};
-    language-powerquery     = self.callPackage ./language-powerquery.nix {};
-    pbix                    = self.callPackage ./pbix.nix {};
-    m2wasm                  = self.callPackage ./m2wasm.nix {};
-  });
+  haskellPackages = pkgs.haskell.packages.${compiler}.override (old: {
+    overrides = pkgs.lib.composeExtensions old.overrides
+        (self: hspkgs: {
+          lens             = easeIfGHCJS hspkgs.lens;
+          comonad          = easeIfGHCJS hspkgs.comonad;
+          semigroupoids    = easeIfGHCJS hspkgs.semigroupoids;
+          QuickCheck       = easeIfGHCJS hspkgs.QuickCheck;
+          tasty-quickcheck = easeIfGHCJS hspkgs.tasty-quickcheck;
+          scientific       = easeIfGHCJS hspkgs.scientific;
+          temporary        = easeIfGHCJS hspkgs.temporary;
+          #--
+          wasm                    = haskell.lib.dontCheck (haskell.lib.doJailbreak hspkgs.wasm);
+          language-powerquery-ast = hspkgs.callCabal2nix "language-powerquery-ast" "${languagePowerQueryASTSrc}" {};
+          language-powerquery     = hspkgs.callCabal2nix "language-powerquery" "${languagePowerQuerySrc}" {};
+          pbix                    = hspkgs.callCabal2nix "pbix" "${pbixSrc}" {};
+          m2wasm                  = hspkgs.callCabal2nix "m2wasm" "${m2wasmSrc}" {};
+        });
+      });
 
-  haskellEnv = haskellPackages_.ghcWithPackages (ps: with ps; [
+  haskellEnv = haskellPackages.ghcWithPackages (ps: with ps; [
     language-powerquery-ast
     language-powerquery
     pbix
