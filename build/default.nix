386c3241453957f2efc7cccea3f3a0d1fff0f924
Barak Bercovitz <barakber@gmail.com>
2019-06-28 06:57:35 +0300

    ghcjs support

--- build/default.nix
+++ build/default.nix
@@ -4,7 +4,20 @@
 }:
 with nixpkgs;
 let
+  easeIfGHCJS = package:
+    if lib.hasPrefix "ghcjs" compiler
+    then haskell.lib.dontHaddock (haskell.lib.dontCheck package)
+    else package;
+
   haskellPackages_ = pkgs.haskell.packages.${compiler}.extend (self: super: rec {
+    lens             = easeIfGHCJS super.lens;
+    comonad          = easeIfGHCJS super.comonad;
+    semigroupoids    = easeIfGHCJS super.semigroupoids;
+    QuickCheck       = easeIfGHCJS super.QuickCheck;
+    tasty-quickcheck = easeIfGHCJS super.tasty-quickcheck;
+    scientific       = easeIfGHCJS super.scientific;
+    temporary        = easeIfGHCJS super.temporary;
+    #--
     wasm                    = self.callPackage ./wasm.nix {};
     language-powerquery-ast = self.callPackage ./language-powerquery-ast.nix {};
     language-powerquery     = self.callPackage ./language-powerquery.nix {};
